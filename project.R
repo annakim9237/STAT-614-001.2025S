5d2e7a9bcef3efacbe63a196e1c6e19113da5414
Hyunjung_anna_ <77224876+annakim9237@users.noreply.github.com>
2025-04-15 14:12:53 -0400

    anti_join

--- project.R
+++ project.R
@@ -1,1 +1,12 @@
-Hi this is test
+options(scipen=100,digits=8) 
+library(Sleuth3) 
+library(tidyverse)
+library(mosaic) 
+library(gridExtra)
+library(gmodels)
+
+df1 <- read_csv("WHR_2015.csv", head(TRUE))
+df2 <- read_csv("WHR_2020.csv", head(TRUE))
+
+anti_join(df1, df2, by="country")
+anti_join(df2, df1, by="country")
